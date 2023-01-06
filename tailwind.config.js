a92ae5662c2a0410da94d39f9d6e683e314f8215
FrederickBizzardo <nyembofred@gmail.com>
2023-01-06 04:03:04 +0200

    Just image, not button added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,12 +13,14 @@ module.exports = {
       },
       colors: {
         'drakGray': '#292929',
+        'lightGray': '#707070',
       },
       padding: {
         'new1': '0.5rem 0',
         'new2': '5rem 0',
-        'new3': '3rem 5rem',
+        'new3': '2rem 3rem',
         'new4': '0 2rem',
+        'new5': '0.5rem 3rem',
       }
     },
   },
