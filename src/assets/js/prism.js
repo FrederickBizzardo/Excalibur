432db02911a835a64498426a8d99092fe61ecacc
FrederickBizzardo <nyembofred@gmail.com>
2023-01-10 20:30:52 +0200

    Editor working needs font fixing

--- src/assets/js/prism.js
+++ src/assets/js/prism.js
@@ -21549,3 +21549,13 @@ function check_tab(element, event) {
   }
 }
 
+const textarea = document.querySelector('textarea')
+const lineNumbers = document.querySelector('.line-numbers')
+
+textarea.addEventListener('keyup', event => {
+  const numberOfLines = event.target.value.split('\n').length
+
+  lineNumbers.innerHTML = Array(numberOfLines)
+    .fill('<span></span>')
+    .join('')
+})
