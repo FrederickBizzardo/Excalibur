5fb81e0415f42b499464cfc722cdc27f04273fa2
FrederickBizzardo <nyembofred@gmail.com>
2023-01-11 17:45:25 +0200

    Placeholder color fixed

--- src/assets/addon/display/placeholder.js
+++ src/assets/addon/display/placeholder.js
@@ -39,7 +39,7 @@
   function setPlaceholder(cm) {
     clearPlaceholder(cm);
     var elt = cm.state.placeholder = document.createElement("pre");
-    elt.style.cssText = "height: 0; overflow: visible";
+    elt.style.cssText = "height: 0; overflow: visible; color: #ACACAC";
     elt.style.direction = cm.getOption("direction");
     elt.className = "CodeMirror-placeholder CodeMirror-line-like";
     var placeHolder = cm.getOption("placeholder")
