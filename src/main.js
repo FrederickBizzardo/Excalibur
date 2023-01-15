ece8ffa54c1c97cc7d6bf511e9762d6f031a277a
FrederickBizzardo <nyembofred@gmail.com>
2023-01-16 00:28:12 +0200

    trying to fix

---
+++ src/main.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    /*titleBarStyle: 'hidden',
+    titleBarOverlay: true,*/
+    width: 800,
+    height: 600,
+    minWidth: 800,
+    minHeight:600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Declare all menu
+  /*const menu_list = [
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "Open File...",
+          click: function () {
+            console.log("Open File Clicked");
+          },
+        },
+      ],
+    },
+  ];
+  const menu_design = Menu.buildFromTemplate(menu_list);
+  Menu.setApplicationMenu(menu_design);*/
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
